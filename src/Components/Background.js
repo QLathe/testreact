4b80080465e4fd5c87318ac02284fc6289102b82
QLathe <quinn.s.lathe@gmail.com>
2021-03-09 14:32:44 -0800

    Components
    Added component folder
    deleted default index view

---
+++ src/Components/Background.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Background extends Component{
+
+    render(){
+        return (
+            <h1>Initial Construct is working</h1>
+        )
+        }
+}
+
+export default Background;
